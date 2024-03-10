56b3a6ccc2602ca094e7005eefc4b0ac5f60bfe9
sharpchen <rui.chen.sharp@gmail.com>
2024-03-10 20:17:46 +0800

    Refactor feature links and add new document names

--- docs/data/Features.data.ts
+++ docs/data/Features.data.ts
@@ -10,21 +10,17 @@ features:
     details: SQL syntax for beginners with MySQL
     icon: 📝
     linkText: Get started
-    link: /sql
   - title: Docker
     details: Ultimate Docker
     icon: 🐋
-    link: /docker
     linkText: Get started
-  - title: CSharp Design Patterns
+  - title: C# Design Patterns
     details: Design Patterns in C#
     icon: 👾
-    link: /csharpdesignpatterns
     linkText: Get started
   - title: JavaScript
     details: JavaScript for C# developer
-    icon: 🦖
-    link: /javascript
+    icon: 😅
     linkText: Get started
   - title: TypeScript
     details: TypeScript for C# developer
@@ -39,22 +35,26 @@ features:
   - title: Python
     details: Python for C# developer
     icon: 🐍
-    link: /
     linkText: Get started
   - title: Vue3
     details: Vue3 for .NET blazor developer
     icon: ⚡
-    link: /vue3
     linkText: Get started
   - title: CesiumJS
     details: CesiumJS with vue3+vite
     icon: 🌏
-    link: /cesiumjs
     linkText: Get started
   - title: Mars3D
     details:
     icon: 🌟
-    link: /cesiumjs
+    linkText: Get started
+  - title: VBA
+    details: VBA for excel
+    icon: 💩
+    linkText: Get started
+  - title: Modern C#
+    details: Modernized C#
+    icon: 🐱‍👤
     linkText: Get started
 ---
 `;
@@ -67,56 +67,76 @@ features:
     #link: /document/Articles/docs/Start your first npm package - Build, CI and Publish.md
 ---`;
 const getIndexLink = (title: string): string | undefined => {
-    const docs = documentRoot()
-        .getDirectories()
-        .find(x => x.name.toLowerCase() === title.toLowerCase())
-        ?.getDirectories()
-        .find(x => x.name === 'docs');
-    if (!docs) return;
-    if (docs.getDirectories().length > 0) {
-        const folder = Enumerable.from(docs.getDirectories())
-            .where(x => x.getFiles().length > 0)
-            .orderBy(x => x.name)
-            .firstOrDefault();
-        const file = folder?.getFiles()[0];
-        return `${documentRoot().name}/${title}/docs/${folder?.name}/${Path.GetFileNameWithoutExtension(file?.name!)}`;
-    }
-    if (docs.getFiles().length > 0) {
-        const file = Enumerable.from(docs.getFiles())
-            .orderBy(x => x.name)
-            .firstOrDefault();
-        return `${documentRoot().name}/${title}/docs/${Path.GetFileNameWithoutExtension(file?.name!)}`;
-    }
+  const docs = documentRoot()
+    .getDirectories()
+    .find(x => x.name.toLowerCase() === title.toLowerCase())
+    ?.getDirectories()
+    .find(x => x.name === 'docs');
+  if (!docs) return;
+  // has multiple chapters
+  if (docs.getDirectories().length > 0) {
+    const folder = Enumerable.from(docs.getDirectories())
+      .where(x => x.getFiles().length > 0)
+      .orderBy(x => x.name)
+      .firstOrDefault();
+    const file = folder?.getFiles()[0];
+    return `${documentRoot().name}/${title}/docs/${folder?.name}/${Path.GetFileNameWithoutExtension(
+      file?.name!
+    )}`;
+  }
+  // no chapter
+  if (docs.getFiles().length > 0) {
+    const file = Enumerable.from(docs.getFiles())
+      .orderBy(x => x.name)
+      .firstOrDefault();
+    return `${documentRoot().name}/${title}/docs/${Path.GetFileNameWithoutExtension(file?.name!)}`;
+  }
 };
-function addLinkToFeature(features: Feature[]): Feature[] {
-    const names = documentRoot()
-        .getDirectories()
-        .map(x => x.name);
-    for (const key in features) {
-        if (Object.prototype.hasOwnProperty.call(features, key)) {
-            const feature = features[key];
-            const match = names.find(x => x.toLowerCase() === feature.title.toLowerCase());
-            if (match) {
-                const link = getIndexLink(
-                    // cs design pattern has conflict that I just leave it with a simple solution.
-                    feature.title.includes('CSharp') ? feature.title.replace('CSharp', 'Csharp') : feature.title
-                );
-                feature.link = link ? link : '/';
-            }
-        }
+function addLinkToFeatures(features: Feature[]): Feature[] {
+  /**
+   * folder names
+   */
+  const names = documentRoot()
+    .getDirectories()
+    .map(x => x.name);
+  for (const key in features) {
+    if (Object.prototype.hasOwnProperty.call(features, key)) {
+      const feature = features[key];
+      /**
+       * Handel exceptions for C#
+       * @param name folder name
+       * @returns handled
+       */
+      const predicate = (name: string) => {
+        const replaced = name.toLowerCase().includes('csharp')
+          ? name.toLowerCase().replace('csharp', 'C#')
+          : name;
+        return feature.title.toLowerCase() === replaced.toLowerCase();
+      };
+      const match = names.find(x => predicate(x));
+      if (match) {
+        // cs design pattern has conflict that I just leave it with a simple solution.
+        const title = feature.title.includes('C#')
+          ? feature.title.replace('C#', 'CSharp')
+          : feature.title;
+        const link = getIndexLink(title);
+        feature.link = link ? link : '/';
+      }
     }
-    return features;
+  }
+  return features;
 }
-const featuresItems: Feature[] = addLinkToFeature(matter(featuresLiteral).data.features);
-const articleFeature: Feature[] = addLinkToFeature(matter(articleLiteral).data.features);
+
+const featuresItems: Feature[] = addLinkToFeatures(matter(featuresLiteral).data.features);
+const articleFeature: Feature[] = addLinkToFeatures(matter(articleLiteral).data.features);
 const loader = {
-    load: (): FeatureCompose => ({ features: featuresItems, articleFeature: articleFeature }),
+  load: (): FeatureCompose => ({ features: featuresItems, articleFeature: articleFeature }),
 };
 
 export default loader;
 type FeatureCompose = {
-    features: Feature[];
-    articleFeature: Feature[];
+  features: Feature[];
+  articleFeature: Feature[];
 };
 
 export declare const data: FeatureCompose;
--- docs/data/global.data.ts
+++ docs/data/global.data.ts
@@ -1,4 +1,22 @@
 import { version } from '../../node_modules/vitepress/package.json';
+
+const documentNames = [
+    'Articles',
+    'Avalonia',
+    'CesiumJS',
+    'Csharp Design Patterns',
+    'Docker',
+    'Git',
+    'JavaScript',
+    'Mars3D',
+    'Modern CSharp',
+    'SQL',
+    'TypeScript',
+    'VBA',
+    'Vue3',
+] as const;
+export type DocumentName = (typeof documentNames)[number];
+
 const globalData = {
     vitepressVersion: version,
 } as const;
