d4eb6eb7bc46261ba3e34bb0bf11e4cf37593412
sharpchen <rui.chen.sharp@gmail.com>
2024-04-11 14:58:24 +0800

    enhance theme syncing

--- .github/workflows/beforeBuild/sync-themes.mjs
+++ .github/workflows/beforeBuild/sync-themes.mjs
@@ -5,10 +5,14 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 export const themes = {
-    'Eva Dark': 'https://raw.githubusercontent.com/sharpchen/Eva-Theme/variant/themes/Eva-Dark.json',
-    'Eva Light': 'https://raw.githubusercontent.com/sharpchen/Eva-Theme/variant/themes/Eva-Light.json',
+  'Eva Dark': 'https://raw.githubusercontent.com/sharpchen/Eva-Theme/variant/themes/Eva-Dark.json',
+  'Eva Light':
+    'https://raw.githubusercontent.com/sharpchen/Eva-Theme/variant/themes/Eva-Light.json',
 };
-export const jsonOutDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../../docs/public/');
+export const jsonOutDir = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  '../../../docs/public/'
+);
 
 /**
  *
@@ -16,35 +20,39 @@ export const jsonOutDir = path.join(path.dirname(fileURLToPath(import.meta.url))
  * @returns {Promise<import('./types.mjs').TextmateTheme | undefined>}
  */
 export async function getThemeJson(url) {
-    try {
-        const response = await axios.get(url);
-        const text = response.data;
-        /**
-         * @type {import('./types.mjs').TextmateTheme}
-         */
-        const json = JSONC.parse(text);
-        return {
-            name: json.name,
-            // @ts-ignore
-            tokenColors: json.tokenColors.map(r => {
-                return { scope: r.scope, settings: r.settings };
-            }),
-        };
-    } catch (error) {
-        console.warn(`Fetch ${url} failed!`, error);
-        return;
-    }
+  try {
+    const response = await axios.get(url);
+    const text = response.data;
+    /**
+     * @type {import('./types.mjs').TextmateTheme}
+     */
+    const json = JSONC.parse(text);
+    return {
+      name: json.name,
+      // @ts-ignore
+      tokenColors: json.tokenColors.map(r => {
+        return { scope: r.scope, settings: r.settings };
+      }),
+    };
+  } catch (error) {
+    console.warn(`Fetch ${url} failed!`, error);
+    return;
+  }
 }
 
 export async function syncAllThemes() {
-    for (const name in themes) {
-        if (Object.hasOwnProperty.call(themes, name)) {
-            const url = themes[name];
-            const themeJson = await getThemeJson(url);
-            if (themeJson) {
-                await fs.writeFile(path.join(jsonOutDir, name + '.json'), JSON.stringify(themeJson, null, 2));
-                console.log(`Successfully synced theme: ${name}`);
-            }
-        }
-    }
+  await Promise.all(
+    Object.entries(themes).map(async x => {
+      const url = x[1];
+      const themeJson = await getThemeJson(url);
+      const name = x[0];
+      if (themeJson) {
+        await fs.writeFile(
+          path.join(jsonOutDir, name + '.json'),
+          JSON.stringify(themeJson, null, 2)
+        );
+        console.log(`Successfully synced theme: ${name}`);
+      }
+    })
+  );
 }
